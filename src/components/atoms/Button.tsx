6d8b357af1ed6867e2e2a0dd951896fdcdc79db7
Krzysztof Król <krzysztof.krol@royalcode.eu>
2024-02-20 13:54:58 +0100

    Even category buttons height

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = ({ label, icon, variant = 'default', type = 'normal', onClick }) => {
-  const baseStyle = `flex items-center gap-2 px-9 md:px-6 lg:px-9 ${type === "normal" ? "py-2.5" : "py-4"} rounded-full text-base font-medium transition`;
+  const baseStyle = `flex items-center gap-2 px-9 md:px-6 lg:px-9 ${type === "normal" ? "py-2.5 h-[47px] lg:h-[41px]" : "py-4"} rounded-full text-base font-medium transition`;
   const styles = {
     default: "bg-white text-brand-gray hover:bg-brand-gray hover:text-white",
     active: "bg-brand-gray-dark text-white hover:bg-brand-gray hover:text-white",
