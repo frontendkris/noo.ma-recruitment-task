c0dbfae7c665e0019a1f8c659dba0658a592331c
Krzysztof Król <krzysztof.krol@royalcode.eu>
2024-03-14 16:00:34 +0100

    Fix link hover in credits

--- src/components/atoms/Credits.tsx
+++ src/components/atoms/Credits.tsx
@@ -1,10 +1,25 @@
 const Credits: React.FC = () => {
   return (
     <p className="text-brand-gray-light absolute right-4 bottom-2 lg:right-10 lg:bottom-10 flex flex-col items-end z-0">
-      <span><span className="mr-2 lg:mr-7">x</span><a href="https://frontendkris.dev" target="_blank" className="text-lg leading-none">frontendkris</a></span>
-      <a href="https://noo.ma" target="_blank" className="text-6xl lg:text-7xl leading-none">noo.ma</a>
+      <span>
+        <span className="mr-2 lg:mr-7">x</span>
+        <a
+          href="https://frontendkris.dev"
+          target="_blank"
+          className="relative text-lg leading-none z-10"
+        >
+          frontendkris
+        </a>
+      </span>
+      <a
+        href="https://noo.ma"
+        target="_blank"
+        className="text-6xl lg:text-7xl leading-none"
+      >
+        noo.ma
+      </a>
     </p>
-  )
-}
+  );
+};
 
 export default Credits;
